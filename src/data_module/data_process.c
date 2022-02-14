66712f065a8801955f7e0ae332f75b8908fb6766
Arryn Pallas <arrynpal@student.21-school.ru>
2022-02-14 09:41:34 +0000

    data_process.c

--- src/data_module/data_process.c
+++ src/data_module/data_process.c
@@ -1,4 +1,5 @@
 #include "data_process.h"
+#include "../data_libs/data_stat.h"
 #include <math.h>
 
 int normalization(double *data, int n)
@@ -21,23 +22,4 @@ int normalization(double *data, int n)
     }
     
     return result;
-}
-
-int input(double *data, int *n) {
-    char c1;
-    if (scanf("%d%c", n, &c1) != 2) return 0;
-    if ((int) c1 != 10) return 0;
-
-    for (int i = 0; i < n; i++) {
-        char c;
-        if (scanf("%lf%c", &data[i], &c) != 2) return 0;
-        if (c != 32 && i < *n - 1) return 0;
-        if (c != 10 && i == *n - 1) return 0;
-    }
-    return 1;
-}
-
-void output(double *data, int n) {
-    for (int i = 0; i < n; i++)
-        printF("%.2df", data[i]);
 }
