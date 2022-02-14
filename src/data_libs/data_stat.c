a1bb1bd89faa2f00b2814e53c10a00bc84b2e2b1
Arryn Pallas <arrynpal@student.21-school.ru>
2022-02-14 09:46:07 +0000

    data_stat.c

--- src/data_libs/data_stat.c
+++ src/data_libs/data_stat.c
@@ -1,1 +1,19 @@
 #include "data_stat.h"
+
+double max(double *data, int n) {
+    double d = data[0];
+    for (int i = 1; i < n; i++)
+        if (data[i] > d)
+            d = data[i];
+    return d;
+}
+double min(double *data, int n) {
+    double d = data[0];
+    for (int i = 1; i < n; i++)
+        if (data[i] < d)
+            d = data[i];
+    return d;
+}
+double mean(double *data, int n) {}
+double variance(double *data, int n) {}
+
