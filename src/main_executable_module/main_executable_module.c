7ae498cef4a5e863141a0e382f449f8a00e4a252
Arryn Pallas <arrynpal@student.21-school.ru>
2022-02-14 11:32:47 +0000

    main_executable_module.c

--- src/main_executable_module/main_executable_module.c
+++ src/main_executable_module/main_executable_module.c
@@ -1,3 +1,12 @@
+#include "../data_module/data_process.h"
+#include "../data_libs/data_io.h"
+#include "../data_libs/data_stat.h"
+#include "data_executable.h"
+
+#include "../data_module/data_process.c"
+#include "../data_libs/data_io.c"
+#include "../data_libs/data_stat.c"
+
 void main()
 {
     double *data;
@@ -14,11 +23,11 @@ void main()
     output(data, n);
     
     printf("\nSORTED NORMALIZED DATA:\n\t");
-    sort(data, n);
+    quick_sort(data, 0, n - 1)
     output(data, n);
     
     printf("\nFINAL DECISION:\n\t");
-    //make_decision(data, n);
-    //...
+    make_decision(data, n);
+    output(data, n);
     
 }
